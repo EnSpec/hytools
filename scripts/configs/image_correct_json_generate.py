28ad4fb31c33427d1aaf7484fb128f0f114b8895
adamchlus <23018310+adamchlus@users.noreply.github.com>
2021-02-12 16:04:59 -0600

    Added more documentation

--- scripts/configs/image_correct_json_generate.py
+++ scripts/configs/image_correct_json_generate.py
@@ -1,11 +1,16 @@
+'''Template script for generating image_correct configuration JSON files.
+
+    These settiing are meant only as an example, are not appropriate for
+    all situations and may need to be adjusted
+'''
+
 import os
 import json
 import glob
 import numpy as np
 
-home = os.path.expanduser("~")
-
-config_file = "/home/chlus/dev_hytools/ic_config.json"
+#Output path for configuration file
+config_file = ".json"
 config_dict = {}
 
 #Only coefficients for good bands will be calculated
@@ -15,33 +20,33 @@ config_dict['bad_bands'] =[[300,400],[900,2600]]  # Subset for testing
 # Input data settings for NEON
 #################################################################
 # config_dict['file_type'] = 'neon'
-# images= glob.glob("/home/chlus/dev_hytools/data/srer/*.h5")
+# images= glob.glob("*.h5")
 # images.sort()
 # config_dict["input_files"] = images
 
 # Input data settings for ENVI
 #################################################################
-# ''' Only difference between ENVI and NEON settings is the specification
-# of the ancillary datasets (ex. viewing and solar geometry). All hytools
-# functions assume that the ancillary data and the image date are the same
-# size, spatially, and are ENVI formatted files.
+''' Only difference between ENVI and NEON settings is the specification
+of the ancillary datasets (ex. viewing and solar geometry). All hytools
+functions assume that the ancillary data and the image date are the same
+size, spatially, and are ENVI formatted files.
 
-# The ancillary parameter is a dictionary with a key per image. Each value
-# per image is also a dictionary where the key is the dataset name and the
-# value is list consisting of the file path and the band number.
+The ancillary parameter is a dictionary with a key per image. Each value
+per image is also a dictionary where the key is the dataset name and the
+value is list consisting of the file path and the band number.
 
-# '''
+'''
 
 config_dict['file_type'] = 'envi'
 aviris_anc_names = ['path_length','sensor_az','sensor_zn',
                     'solar_az', 'solar_zn','phase','slope',
                     'aspect', 'cosine_i','utc_time']
-images= glob.glob("/home/chlus/dev_hytools/data/yose/*img")
+images= glob.glob("*img")
 images.sort()
 config_dict["input_files"] = images
 
 config_dict["anc_files"] = {}
-anc_files = glob.glob("/home/chlus/dev_hytools/data/yose/*ort")
+anc_files = glob.glob("*ort")
 anc_files.sort()
 for i,image in enumerate(images):
     config_dict["anc_files"][image] = dict(zip(aviris_anc_names,
