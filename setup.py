ea63fa7f164d5ed3f70ef0e9590597746a67bacf
adamchlus <23018310+adamchlus@users.noreply.github.com>
2021-02-15 08:26:37 -0600

    Added missing dependency

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     download_url = 'https://github.com/EnSpec/hytools/archive/1.0.0.tar.gz',
     author = 'Adam Chlus, Zhiwei Ye, Ting Zheng, Natalie Queally and Philip Townsend',
     packages=find_packages(),
-    install_requires=['h5py>=3.1.0',
-                      'matplotlib>=3.3.3',
-                      'numpy>=1.19.4',
-                      'pandas>=1.1.0',
+    install_requires=['h5py',
+                      'matplotlib',
+                      'numpy',
+                      'pandas',
                       'ray',
-                      'scikit-learn>=0.23.0',
-                      'scipy>=1.5.3']
+                      'scikit-learn',
+                      'scipy']
     )
 
 
