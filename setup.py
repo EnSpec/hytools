0cd7fe7d85ac4dc3cf11845d0f0f70159cd27d99
adamchlus <23018310+adamchlus@users.noreply.github.com>
2021-02-15 08:23:17 -0600

    Added missing dependency

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
                       'matplotlib>=3.3.3',
                       'numpy>=1.19.4',
                       'pandas>=1.1.0',
+                      'ray',
                       'scikit-learn>=0.23.0',
                       'scipy>=1.5.3']
     )
