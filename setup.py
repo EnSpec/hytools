d0e01ca8e50433a801866194d3c3f4134b66c8b8
adamchlus <23018310+adamchlus@users.noreply.github.com>
2021-03-05 11:08:58 -0600

    Updated python version requirements and HyTools version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='hy-tools',
     description= 'HyTools: Hyperspectral image processing library',
-    version='1.0.2',
+    version='1.1.0',
     license='MIT',
     url='https://github.com/EnSpec/hytools',
     author = 'Adam Chlus, Zhiwei Ye, Ting Zheng, Natalie Queally and Philip Townsend',
@@ -14,7 +14,8 @@ setup(
                       'pandas',
                       'ray',
                       'scikit-learn',
-                      'scipy']
+                      'scipy'],
+    python_requires='>=3.6, !=3.9.*'
     )
 
 
