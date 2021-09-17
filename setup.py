aa8b0c8df0f87144f5493af8327bf0328ba35ad0
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-09-17 13:17:21 -0400

    Updated version, license and authors

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='hy-tools',
     description= 'HyTools: Hyperspectral image processing library',
-    version='1.1.2',
-    license='MIT',
+    version='1.3',
+    license='GNU General Public License v3.0',
     url='https://github.com/EnSpec/hytools',
-    author = 'Adam Chlus, Zhiwei Ye, Ting Zheng, Natalie Queally and Philip Townsend',
+    author = 'Adam Chlus, Zhiwei Ye, Ting Zheng, Natalie Queally, Evan Greenberg and Philip Townsend',
     packages=find_packages(),
     install_requires=['h5py',
                       'matplotlib',
