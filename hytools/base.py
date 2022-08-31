e84c90558a03719b133bd7d6bf57e6f180b6585a
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-08-31 15:27:26 -0700

    Made ENVI default file format

--- hytools/base.py
+++ hytools/base.py
@@ -80,7 +80,7 @@ class HyTools:
         self.wavelength_units = None
         self.wavelengths = []
 
-    def read_file(self,file_name,file_type,anc_path = None, ext = False):
+    def read_file(self,file_name,file_type = 'envi',anc_path = None, ext = False):
         self.file_name = file_name
         self.file_type = file_type
 
