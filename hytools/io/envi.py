5a837ce76fad81737e81d0336a869dd01fec3cb5
oldmanye007 <xtpaye@gmail.com>
2024-08-30 19:57:29 -0500

    No data guessing from four corners according to interleaves

--- hytools/io/envi.py
+++ hytools/io/envi.py
@@ -151,10 +151,22 @@ def open_envi(hy_obj,anc_path = {}, ext = False):
     # If no_data value is not specified guess using image corners.
     if hy_obj.no_data is None:
         hy_obj.load_data()
-        up_l = hy_obj.data[0,0,0]
-        up_r = hy_obj.data[0,-1,0]
-        low_l = hy_obj.data[-1,0,0]
-        low_r = hy_obj.data[-1,-1,0]
+        if header_dict["interleave"] == 'bip':
+            up_l = hy_obj.data[0,0,0]
+            up_r = hy_obj.data[0,-1,0]
+            low_l = hy_obj.data[-1,0,0]
+            low_r = hy_obj.data[-1,-1,0]
+        elif header_dict["interleave"] == 'bil':
+            up_l = hy_obj.data[0,0,0]
+            up_r = hy_obj.data[0,0,-1]
+            low_l = hy_obj.data[-1,0,0]
+            low_r = hy_obj.data[-1,0,-1]
+        elif header_dict["interleave"] == 'bsq':
+            up_l = hy_obj.data[0,0,0]
+            up_r = hy_obj.data[0,0,-1]
+            low_l = hy_obj.data[0,-1,0]
+            low_r = hy_obj.data[0,-1,-1]
+
 
         if hy_obj.endianness != sys.byteorder:
             up_l = up_l.byteswap()
